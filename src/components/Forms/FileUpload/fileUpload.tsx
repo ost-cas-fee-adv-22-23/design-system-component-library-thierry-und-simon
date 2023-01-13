985bc22fe778c67b29803a795442596c3c194a58
eoss-sh <sh@eoss.ch>
2023-01-13 21:38:11 +0100

    styling of file upload component

--- src/components/Forms/FileUpload/fileUpload.tsx
+++ src/components/Forms/FileUpload/fileUpload.tsx
@@ -15,21 +15,27 @@ export const FileUpload: FC<FileUploadProps> = ({
   placeholder,
 }) => {
   return (
-    <div>
-      <div className="flex items-center justify-center w-full">
-        <label
-          htmlFor="drop"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded"
-        >
-          <Icon type={IconType.upload} color="#64748B" />
-          <input id="dropzone-file" type="file" className="hidden" />
-        </label>
-      </div>
-      <input
-        className="bg-slate-50 p-2 text-[20px] text-slate-700 font placeholder:text-slate-500 rounded border border-slate-200 hover:border-slate-300 hover:border-2 focus:border-2 focus:outline-none focus:border-violet-600"
-        type="file"
-        placeholder="...oder Datei auswählen"
-      />
+    <div className="flex flex-col items-center justify-center w-full mx-l">
+      <label
+        htmlFor="drop"
+        className="flex flex-col items-center justify-center w-full h-64 border-2 border-slate-200 border-dashed rounded cursor-pointer bg-slate-100"
+      >
+        <Icon type={IconType.upload} color="#64748B" />
+        <span className="text-[24px] text-bold text-slate-500">
+          Datei hierhin ziehen...
+        </span>
+        <span className="text-slate-400">JPEG oder PNG, maximal 50 MB</span>
+        <input id="dropzone-file" type="file" className="hidden" />
+      </label>
+
+      <label
+        className="flex flex-row justify-center w-full bg-slate-300 py-3 mt-4 text-sm text-slate-500 font placeholder:text-slate-500 rounded border border-slate-200 hover:border-slate-300 hover:border-2 focus:border-2 focus:outline-none focus:border-violet-600"
+        htmlFor="file-upload"
+      >
+        <span className="mr-[9px]">...oder Datei auswählen</span>
+        <Icon type={IconType.upload} color="#64748B" />
+      </label>
+      <input id="file-upload" className="hidden" type="file" />
     </div>
   );
 };
