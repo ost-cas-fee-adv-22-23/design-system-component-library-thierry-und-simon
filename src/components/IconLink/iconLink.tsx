6a75ad23d9d047a87da11047aa93e6f7f9481e42
eoss-sh <sh@eoss.ch>
2023-01-14 22:31:29 +0100

    added classes obkect to move all css classes and style logic to seperate object

--- src/components/IconLink/iconLink.tsx
+++ src/components/IconLink/iconLink.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react';
-import { Icon, IconType } from '../Icon/Icon';
+import React, { FC, useState } from "react";
+import { Icon, IconType } from "../Icon/Icon";
+
+type HoverType = "false" | "true";
 
 export enum IconLinkType {
-  slate = 'slate-400',
-  violet = 'violet-600',
+  slate = "slate-400",
+  violet = "violet-600",
 }
 
 export type IconLinkProps = {
@@ -13,28 +15,40 @@ export type IconLinkProps = {
   textSize: string;
 };
 
-export const IconLink: FC<IconLinkProps> = ({ type, icon, text, textSize }) => {
-  const [hover, setHover] = useState(false);
+const classes = {
+  wrapperGeneral: "flex items-center gap-x-1",
+  wrapperColor: (type: IconLinkType) => {
+    const classesMap = {
+      [IconLinkType.slate]: "text-slate-400 hover:text-slate-600",
+      [IconLinkType.violet]: "text-violet-600 hover:text-violet-900",
+    };
+    return classesMap[type];
+  },
+  IconColor: (type: IconLinkType, hover: HoverType) => {
+    const classesMap = {
+      [IconLinkType.slate]: {
+        false: "#94A3B8",
+        true: "#475569",
+      },
+      [IconLinkType.violet]: {
+        false: "#7C3AED",
+        true: "#4C1D95",
+      },
+    };
+    return classesMap[type][hover];
+  },
+};
 
-  const color =
-    type === IconLinkType.slate && !hover
-      ? '#94A3B8'
-      : type === IconLinkType.slate && hover
-      ? '#475569'
-      : type !== IconLinkType.slate && hover
-      ? '#4C1D95'
-      : '#7C3AED';
+export const IconLink: FC<IconLinkProps> = ({ type, icon, text, textSize }) => {
+  const [hover, setHover] = useState<HoverType>("false");
+
   return (
     <div
-      className={`flex items-center gap-x-1 ${
-        type === IconLinkType.slate
-          ? 'text-slate-400 hover:text-slate-600'
-          : 'text-violet-600 hover:text-violet-900'
-      }`}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
+      className={`${classes.wrapperGeneral} ${classes.wrapperColor(type)}`}
+      onMouseEnter={() => setHover("true")}
+      onMouseLeave={() => setHover("false")}
     >
-      <Icon type={icon} color={color} />
+      <Icon type={icon} color={classes.IconColor(type, hover)} />
       <p className={textSize}>{text}</p>
     </div>
   );
