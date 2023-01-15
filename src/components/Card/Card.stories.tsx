7f8817828af1c7776a4f9ae0c7a6400615d0a533
Thierry Seiler <>
2023-01-15 17:06:23 +0100

    an cards gearbeitet

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -3,7 +3,9 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Card } from './Card';
 import { SizeType, User } from '../User/user';
 import { IconLink, IconLinkType } from '../IconLink/iconLink';
-import { IconType } from '../Icon/Icon';
+import { Icon, IconType } from '../Icon/Icon';
+import { Textarea } from '../Forms/Textarea/textarea';
+import { Button, ButtonSize, ButtonColor } from '../Button/button';
 
 export default {
   title: 'Mocks/Card',
@@ -18,12 +20,59 @@ export default {
 
 const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
 
-export const defaultCard = Template.bind({});
+export const exampleCard = Template.bind({});
+
+export const exampleCardInline = Template.bind({});
 
 export const writeCard = Template.bind({});
 
-defaultCard.args = {
+export const writeCardInline = Template.bind({});
+
+exampleCard.args = {
   showProfileImage: true,
+  children: (
+    <>
+      <User type={SizeType.BASE} />
+
+      <p className='mt-m'>
+        Paragraph – Quia aut et aut. Sunt et eligendi similique enim qui quo
+        minus. Aut aut error velit voluptatum optio sed quis cumque error magni.
+        Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et
+        recusandae quos excepturi ut deleniti ut repellat magni.
+      </p>
+
+      <div className='my-m rounded-lg bg-violet-200 w-100 w-100 pt-16/9'></div>
+      <div className='flex'>
+        <IconLink
+          type={IconLinkType.slate}
+          icon={IconType.bubble}
+          text='Message'
+          textSize='text-sm'
+        />
+        <div className='ml-xl'>
+          <IconLink
+            type={IconLinkType.slate}
+            icon={IconType.like}
+            text='Like'
+            textSize='text-sm'
+          />
+        </div>
+        <div className='ml-xl'>
+          <IconLink
+            type={IconLinkType.slate}
+            icon={IconType.share}
+            text='Copy Link'
+            textSize='text-xs'
+          />
+        </div>
+      </div>
+    </>
+  ),
+};
+
+exampleCardInline.args = {
+  roundedBorders: false,
+  showProfileImge: false,
   children: (
     <>
       <User type={SizeType.BASE} />
@@ -68,7 +117,66 @@ writeCard.args = {
   showProfileImage: true,
   children: (
     <>
-      <h4>Hey, was läuft?</h4>
+      <h4 className='mb-s'>Hey, was läuft?</h4>
+      <Textarea placeholder='Deine Meinung zählt!'></Textarea>
+      <div className='flex mt-xs'>
+        <Button
+          size={ButtonSize.medium}
+          color={ButtonColor.slate}
+          grow={true}
+          label='Bild hochladen'
+        >
+          <span className='ml-xs'>
+            <Icon type={IconType.upload} color='white' />
+          </span>
+        </Button>
+        <Button
+          size={ButtonSize.medium}
+          color={ButtonColor.violet}
+          grow={true}
+          label='Absenden'
+        >
+          <span className='ml-xs'>
+            <Icon type={IconType.send} color='white' />
+          </span>
+        </Button>
+      </div>
+    </>
+  ),
+};
+
+writeCardInline.args = {
+  showProfileImage: false,
+  roundedBorders: false,
+  children: (
+    <>
+      <User type={SizeType.BASE} />
+      <div className='mt-s'>
+        <Textarea placeholder='Deine Meinung zählt!'></Textarea>
+      </div>
+
+      <div className='flex mt-xs'>
+        <Button
+          size={ButtonSize.medium}
+          color={ButtonColor.slate}
+          grow={true}
+          label='Bild hochladen'
+        >
+          <span className='ml-xs'>
+            <Icon type={IconType.upload} color='white' />
+          </span>
+        </Button>
+        <Button
+          size={ButtonSize.medium}
+          color={ButtonColor.violet}
+          grow={true}
+          label='Absenden'
+        >
+          <span className='ml-xs'>
+            <Icon type={IconType.send} color='white' />
+          </span>
+        </Button>
+      </div>
     </>
   ),
 };
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,16 +11,22 @@ export enum CardType {
 
 export type CardProps = {
   showProfileImage: boolean;
+  roundedBorders: boolean;
   children: ReactNode;
 };
 
 export const Card: FC<CardProps> = ({
   showProfileImage,
   children,
+  roundedBorders = true,
   ...props
 }) => {
   return (
-    <div className='d-flex flex-column px-xl py-l rounded-t-xl bg-white relative'>
+    <div
+      className={`d-flex flex-column px-xl py-l bg-white relative ${
+        roundedBorders && 'rounded-t-xl'
+      }`}
+    >
       {showProfileImage && (
         <div className='absolute -left-l'>
           <UserImage type={UserImageSizeType.M} />
