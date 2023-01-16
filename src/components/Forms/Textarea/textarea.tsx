7f497bdc9a11ab9b67c0785b9ff604008cbfa60d
eoss-sh <sh@eoss.ch>
2023-01-16 20:09:58 +0100

    styling fixes textarea, change with and border to outline

--- src/components/Forms/Textarea/textarea.tsx
+++ src/components/Forms/Textarea/textarea.tsx
@@ -3,21 +3,25 @@ import React, { FC } from "react";
 export type TextareaProps = {
   children: React.ReactNode;
   placeholder: string;
-  cols: number;
   rows: number;
+};
+
+const classes = {
+  textarea:
+    "p-4 m-[1px] w-full text-[20px] text-slate-900 placeholder:text-slate-500 bg-slate-100 rounded-lg resize-none outline outline-1 outline-slate-200",
+  textareaHover: "p-4 hover:outline-slate-300 hover:outline-2",
+  textareaFocus: "p-4 focus:outline-2 focus:outline-violet-600",
 };
 
 export const Textarea: FC<TextareaProps> = ({
   children,
   placeholder,
   rows = 5,
-  cols = 30,
 }) => {
   return (
     <textarea
-      className="p-4 text-[20px] text-slate-900 placeholder:text-slate-500 bg-slate-100 rounded-lg resize-none border border-slate-200 hover:border-slate-300 hover:border-2 focus:border-2 focus:outline-none focus:border-violet-600"
+      className={`${classes.textarea} ${classes.textareaHover} ${classes.textareaFocus}`}
       rows={rows}
-      cols={cols}
       placeholder={placeholder}
     >
       {children}
