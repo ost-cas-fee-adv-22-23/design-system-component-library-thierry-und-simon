ecc6f7323289cce5f8aacd5a9ec3816219941ac8
eoss-sh <sh@eoss.ch>
2023-01-22 15:37:16 +0100

    fix: excluded tailwind config theme in seperate file, chagned package.json to export this file so that i can be used in consuming project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,44 +4,7 @@ const plugin = require('tailwindcss/plugin')
 const thierrySimonMumbelConfig = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   safelist: ['text-2xl'],
-  theme: {
-    fontFamily: {
-      sans: ['Poppins', 'sans-serif']
-    },
-    fontSize: {
-      xs: ['14px', '14px'],
-      sm: ['16px', '16px'],
-      base: ['18px', '25.2px'],
-      lg: ['24px', '34.8px'],
-      xl: ['24px', '30px'],
-      '2xl': ['32px', '40px'],
-      '3xl': ['40px', '50px'],
-      '4xl': ['48px', '60px']
-    },
-    extend: {
-      spacing: {
-        xxs: '4px',
-        xs: '8px',
-        s: '16px',
-        m: '24px',
-        l: '32px',
-        xl: '48px',
-        xxl: '64px'
-      },
-      padding: {
-        '3/4': '66.6666%',
-        '16/9': '56.6666%'
-      },
-      borderRadius: {
-        md: '6px',
-        lg: '8px',
-        xl: '16px'
-      },
-      borderWidth: {
-        md: '6px'
-      }
-    }
-  },
+  presets: [require('./mumble-presets.js')],
   plugins: [
     require('@headlessui/tailwindcss'),
     plugin(function ({ addBase, theme }) {
@@ -77,5 +40,3 @@ const thierrySimonMumbelConfig = {
 }
 
 module.exports = thierrySimonMumbelConfig
-// export tailwind config as plugin to be used by other projects when isntalled as npm package
-module.exports.plugin = thierrySimonMumbelConfig
