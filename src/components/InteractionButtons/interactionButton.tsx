95122f37609a6392ddb8ba5541b88c243f0d00bf
Thierry Seiler <>
2023-01-24 14:56:39 +0100

    worked on interaction buttons

--- src/components/InteractionButtons/interactionButton.tsx
+++ src/components/InteractionButtons/interactionButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { IconType, Icon } from '../Icon/icon'
 
 export enum InteractionButtonType {
@@ -14,82 +14,117 @@ type IconLinkProps = {
 
 type ContentProps = {
   count: number
-  color: string
+  color?: string
 }
 
-const LikeContent: FC<ContentProps> = ({ count, color }) => {
+const classes = {
+  wrapperGeneral:
+    'flex items-center gap-x-2 px-3 py-2 rounded-2xl transition-colors duration-300'
+}
+
+const LikeContent: FC<ContentProps> = ({ count }) => {
+  const [liked, setLiked] = useState(false)
+  const [localCount, setLocalCount] = useState(count)
+  const [hover, setHover] = useState(false)
+  console.log(count)
+
+  useEffect(() => {
+    setLocalCount(count)
+  }, [count])
+
+  const handleLike = () => {
+    if (!liked) {
+      setLiked(true)
+      setLocalCount(localCount + 1)
+    } else {
+      setLiked(false)
+      setLocalCount(localCount - 1)
+    }
+  }
+
+  const currentIconColor =
+    hover || (localCount > 0 && liked) ? '#EC4899' : '#475569'
+
   return (
-    <>
+    <button
+      className={`${classes.wrapperGeneral}  hover:bg-pink-50 ${
+        localCount > 0 || liked
+          ? 'text-pink-900 hover:text-pink-600'
+          : 'text-slate-600 hover:text-pink-600'
+      }`}
+      onClick={() => handleLike()}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <Icon
-        type={count === 0 ? IconType.like : IconType.like_dark}
-        color={count === 0 ? color : '#EC4899'}
+        type={localCount === 0 || !liked ? IconType.like : IconType.like_dark}
+        color={currentIconColor}
       />
-      <p
-        className={
-          count === 0
-            ? 'text-slate-600 hover:text-pink-600'
-            : 'text-pink-900 hover:text-pink-600'
-        }
-      >
-        {count === 0 ? 'Like' : `${count} Likes`}
+      <p>
+        {localCount === 0
+          ? 'Like'
+          : `${localCount} Like${localCount > 1 ? 's' : ''}`}
       </p>
-    </>
+    </button>
   )
 }
 
-const CommentContent: FC<ContentProps> = ({ count, color }) => {
+const CommentContent: FC<ContentProps> = ({ count }) => {
+  const [hover, setHover] = useState(false)
+
+  const currentIconColor = hover || count > 0 ? '#7C3AED' : '#475569'
+
   return (
-    <>
+    <button
+      className={`${classes.wrapperGeneral}  hover:bg-violet-50 text-slate-600 hover:text-violet-600`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <Icon
         type={count === 0 ? IconType.bubble : IconType.bubble_dark}
-        color={count === 0 ? color : '#7C3AED'}
+        color={currentIconColor}
       />
-      <p className="text-slate-600 hover:text-violet-600">
-        {count === 0 ? 'Comment' : `${count} Comments`}
-      </p>
-    </>
+      <p>{count === 0 ? 'Comment' : `${count} Comments`}</p>
+    </button>
   )
 }
 
-const CopyContent: FC<ContentProps> = ({ color }) => {
+const CopyContent: FC<ContentProps> = () => {
+  const iconColor = '#475569'
+  const [clicked, setClicked] = useState(false)
+
+  function handleClick() {
+    setClicked(true)
+
+    setTimeout(() => {
+      setClicked(false)
+    }, 1500)
+  }
+
   return (
-    <>
-      <Icon type={IconType.share} color={color} />
-      <p className="text-slate-600">Copy Link</p>
-    </>
+    <button
+      className={`${classes.wrapperGeneral}  hover:bg-slate-100 text-slate-60`}
+      onClick={() => {
+        handleClick()
+      }}
+    >
+      <Icon type={IconType.share} color={iconColor} />
+      <p className="text-slate-600">{clicked ? 'Link copied' : 'Copy Link'}</p>
+    </button>
   )
 }
 
 export const InteractionButton: FC<IconLinkProps> = ({ type, count = 0 }) => {
-  const [hover, setHover] = useState(false)
-  const color =
-    !hover || type === InteractionButtonType.share
-      ? '#475569'
-      : hover && type === InteractionButtonType.like
-      ? '#EC4899'
-      : hover && type === InteractionButtonType.comment
-      ? '#7C3AED'
-      : '#475569'
-
+  console.log(type)
   return (
-    <button
-      className={`flex items-center gap-x-2 px-3 py-2 rounded-2xl ${
-        type === InteractionButtonType.like
-          ? 'hover:bg-pink-50'
-          : type === InteractionButtonType.comment
-          ? 'hover:bg-violet-50'
-          : 'hover:bg-slate-100'
-      }`}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
+    <>
       {type === InteractionButtonType.like ? (
-        <LikeContent count={count} color={color} />
+        <LikeContent count={count} />
       ) : type === InteractionButtonType.comment ? (
-        <CommentContent count={count} color={color} />
+        <CommentContent count={count} />
       ) : (
-        <CopyContent count={count} color={color} />
+        <CopyContent count={count} />
       )}
-    </button>
+    </>
   )
 }
