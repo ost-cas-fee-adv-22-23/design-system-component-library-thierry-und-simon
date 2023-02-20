0e7f2db2b919bbafce9c488cfecd32bdf0823ea8
Thierry Seiler <>
2023-02-20 11:18:56 +0100

    gh79: remove dead code, added dayjs in user component instead of vanilla code

--- src/components/Toggle/toggle.tsx
+++ src/components/Toggle/toggle.tsx
@@ -13,7 +13,7 @@ export const Toggle: FC = () => {
           console.log('Changed selected tab to:', index)
         }}
       >
-        <Tab.List className="rounded-lg bg-slate-200 p-xxs ">
+        <Tab.List className="rounded-lg bg-slate-200 p-xxs">
           <Tab className={`${styledTabs}`}>
             <p>Deine Mumbles</p>
           </Tab>
@@ -21,11 +21,10 @@ export const Toggle: FC = () => {
             <p>Deine Likes</p>
           </Tab>
         </Tab.List>
-        {/* <Tab.Panels>
-          <Tab.Panel>Content 1</Tab.Panel>
-          <Tab.Panel>Content 2</Tab.Panel>
-          <Tab.Panel>Content 3</Tab.Panel>
-        </Tab.Panels> */}
+        <Tab.Panels>
+          <Tab.Panel></Tab.Panel>
+          <Tab.Panel></Tab.Panel>
+        </Tab.Panels>
       </Tab.Group>
     </div>
   )
